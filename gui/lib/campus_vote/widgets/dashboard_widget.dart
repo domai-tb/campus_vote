af69437a4a381ba5bcd10ee025b6dcef3897f794
Domai <github@domai-tb.com>
2024-06-21 18:06:41 +0200

    gui stats

--- gui/lib/campus_vote/widgets/dashboard_widget.dart
+++ gui/lib/campus_vote/widgets/dashboard_widget.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:campus_vote/campus_vote/api/grpc_client.dart';
+import 'package:campus_vote/campus_vote/widgets/ballotbox_stats.dart';
 import 'package:campus_vote/generated/api.pbgrpc.dart';
 import 'package:flutter/material.dart';
 
@@ -44,25 +45,7 @@ class _DashboardState extends State<Dashboard> {
         return Padding(
           padding: const EdgeInsets.all(8),
           child: Column(
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Card.filled(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Text('Election Year: ${stats.electionYear}'),
-                    ),
-                  ),
-                  Card.filled(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Text('Total Votes: ${stats.totalVotes}'),
-                    ),
-                  ),
-                ],
-              ),
-            ],
+            children: buildStats(),
           ),
         );
       },
@@ -72,5 +55,81 @@ class _DashboardState extends State<Dashboard> {
   Future<void> updateStats() async {
     final apiStats = await client.getElectionStats();
     if (mounted) setState(() => stats = apiStats);
+  }
+
+  List<Widget> buildStats() {
+    List<Widget> retWidgets = [
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Card.filled(
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text('Election Year: ${stats.electionYear}'),
+            ),
+          ),
+          Card.filled(
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text('Total Votes: ${stats.totalVotes}'),
+            ),
+          ),
+        ],
+      ),
+      // Header widget
+      Row(
+        children: [
+          Expanded(
+            child: Card.filled(
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Row(
+                  children: [
+                    const Text('BallotBox'),
+                    const SizedBox(width: 50),
+                    Text(getDayName(0)),
+                    const SizedBox(width: 50),
+                    Text(getDayName(1)),
+                    const SizedBox(width: 50),
+                    Text(getDayName(2)),
+                    const SizedBox(width: 50),
+                    Text(getDayName(3)),
+                    const SizedBox(width: 50),
+                    Text(getDayName(4)),
+                    const SizedBox(width: 50),
+                    const Text('Total Votes'),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      )
+    ];
+
+    for (final bbStat in stats.ballotBoxes) {
+      retWidgets.add(
+        BallotboxStatsWidget(stats: bbStat),
+      );
+    }
+
+    return retWidgets;
+  }
+
+  String getDayName(int day) {
+    switch (day) {
+      case 0:
+        return 'Monday';
+      case 1:
+        return 'Tuesday';
+      case 2:
+        return 'Wendsday';
+      case 3:
+        return 'Thursday';
+      case 4:
+        return 'Friday';
+      default:
+        return 'ERROR';
+    }
   }
 }
